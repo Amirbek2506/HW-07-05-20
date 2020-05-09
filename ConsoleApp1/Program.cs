8cf8af279626b1741a8f42b74e8d68d7eb16ad6e
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-05-09 11:33:18 +0500

    finish

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("1.Create\n2.Read\n3.Update\n4.Delete\nВаш выбор: ");
+                switch (Console.ReadLine())
+                {
+                    case "1": CRUD.Create(); break;
+                    case "2": CRUD.Read(); break;
+                    case "3": CRUD.Update(); break;
+                    case "4": CRUD.Delete(); break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
