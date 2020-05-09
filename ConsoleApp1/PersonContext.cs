909b65381e4f7f2daeabd772deb4b13137947561
Amirbek2506 <62469848+Amirbek2506@users.noreply.github.com>
2020-05-09 11:03:30 +0500

    подключим базу данных на проект (с помощю Microsoft.EntityFrameworkCore.SqlServer )

---
+++ ConsoleApp1/PersonContext.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ConsoleApp1
+{
+    public partial class PersonContext : DbContext
+    {
+        public PersonContext()
+        {
+        }
+
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<PersonTable> PersonTable { get; set; }
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=Amirbek;Database=Person;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PersonTable>(entity =>
+            {
+                entity.Property(e => e.BirthDate).HasColumnType("datetime");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.MiddleName).HasMaxLength(200);
+            });
+
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Faculty)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.MiddleName).HasMaxLength(200);
+
+                entity.Property(e => e.Nation)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.PlaceOfBirth)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.PlaceOfResidence)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.Specialty)
+                    .IsRequired()
+                    .HasMaxLength(200);
+
+                entity.Property(e => e.University)
+                    .IsRequired()
+                    .HasMaxLength(200);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ConsoleApp1/Students.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public partial class Students
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public int BirthYear { get; set; }
+        public string Nation { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string PlaceOfBirth { get; set; }
+        public string PlaceOfResidence { get; set; }
+        public string University { get; set; }
+        public string Faculty { get; set; }
+        public string Specialty { get; set; }
+        public int Login { get; set; }
+        public int Password { get; set; }
+    }
+}
